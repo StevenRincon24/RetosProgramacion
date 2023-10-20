a899c9a51d7b45d752d857cf7f7de7845e93c909
StevenRincon24 <hovar.rincon@uptc.edu.co>
2023-10-19 21:30:57 -0500

    Ejercicio 1

---
+++ easy/1.js
@@ -0,0 +1,15 @@
+const gifts = ['cat', 'game', 'socks']
+
+console.log(wrapping(gifts))
+
+function wrapping(gifts) {
+  const wrappedGifs=[]
+  gifts.map(gift =>{
+    const cantidad = gift.length + 2
+    const starts = '*'.repeat(cantidad)
+    
+    const mensaje = starts + "\n*"+`${gift}` +"*\n"+ starts
+    wrappedGifs.push(mensaje)
+  }) 
+  return wrappedGifs
+}
